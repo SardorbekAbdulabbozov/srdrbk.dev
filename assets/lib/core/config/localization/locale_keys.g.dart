a25b7fa9845da5229962db206359b14bfc66051f
SardorbekAbdulabbozov <sabdulabbosov@gmail.com>
2024-08-23 20:01:26 +0500

    first commit

---
+++ assets/lib/core/config/localization/locale_keys.g.dart
@@ -0,0 +1,18 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const about = 'about';
+  static const projects = 'projects';
+  static const contact = 'contact';
+  static const downloadCV = 'downloadCV';
+  static const aboutMe = 'aboutMe';
+  static const skills = 'skills';
+  static const skillsDescription = 'skillsDescription';
+  static const experience = 'experience';
+  static const experienceDescription = 'experienceDescription';
+  static const projectsDescription = 'projectsDescription';
+  static const getInTouch = 'getInTouch';
+  static const contactMethods = 'contactMethods';
+  static const profileOnPlatfroms = 'profileOnPlatfroms';
+
+}
