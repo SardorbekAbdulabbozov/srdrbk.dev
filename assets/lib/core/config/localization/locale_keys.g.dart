07b1ed5317407cb325410e2bcce6db578c76a7c6
Sardorbek Abdulabbozov <sabdulabbosov@gmail.com>
2026-07-04 23:35:03 +0500

    - implemented "blog" and removed "about"

--- assets/lib/core/config/localization/locale_keys.g.dart
+++ assets/lib/core/config/localization/locale_keys.g.dart
@@ -1,8 +1,11 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
+// ignore_for_file: constant_identifier_names
+
 abstract class  LocaleKeys {
   static const about = 'about';
   static const projects = 'projects';
+  static const blogs = 'blogs';
   static const contact = 'contact';
   static const downloadCV = 'downloadCV';
   static const aboutMe = 'aboutMe';
@@ -11,8 +14,11 @@ abstract class  LocaleKeys {
   static const experience = 'experience';
   static const experienceDescription = 'experienceDescription';
   static const projectsDescription = 'projectsDescription';
+  static const blogsDescription = 'blogsDescription';
+  static const viewAll = 'viewAll';
   static const getInTouch = 'getInTouch';
   static const contactMethods = 'contactMethods';
   static const profileOnPlatfroms = 'profileOnPlatfroms';
+  static const readMore = 'readMore';
 
 }
